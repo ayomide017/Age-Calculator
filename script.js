5e61ffdf9503e0573dc4ea01842a185ec99aa704
Adio Ayomide <107922605+ayomide017@users.noreply.github.com>
2024-08-01 17:19:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-1function age() {
+function age() {
     var d1 = document.getElementById('date').value;
     var m1 = document.getElementById('month').value;
     var y1 = document.getElementById('year').value;
